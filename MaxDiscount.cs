27cc2925b63b440489d0f4553456066d533dc113
Mallikharjuna Rao <31948311+2mallikharjuna@users.noreply.github.com>
2023-03-05 19:26:18 +1100

    Add files via upload

---
+++ MaxDiscount.cs
@@ -0,0 +1,122 @@
+﻿
+using System;
+
+class Discount
+{
+    public string Index;
+    public int Type;
+    public int Value;
+    public Discount(string index, int type, int value)
+    {
+        this.Index = index;
+        this.Type = type;
+        this.Value = value;
+    }
+}
+
+class Result
+{
+
+    /*
+     * Complete the 'findLowestPrice' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. 2D_STRING_ARRAY products
+     *  2. 2D_STRING_ARRAY discounts
+     */
+    static int CalcDiscountAmount(int taggedPrice, Discount discount)
+    {
+
+        if (discount.Type == 0)
+        {
+            taggedPrice = discount.Value;
+        }
+        else if (discount.Type == 1)
+        {
+            taggedPrice = (int)Math.Round(taggedPrice * (100.0 - discount.Value) / 100);
+        }
+        else if (discount.Type == 2)
+        {
+            taggedPrice -= discount.Value;
+        }
+
+        return taggedPrice;
+
+    }
+
+    public static int findLowestPrice(List<List<string>> products, List<List<string>> discounts)
+    {
+        int result = 0;
+        Dictionary<string, Discount> productDiscount = new Dictionary<string, Discount>();
+        foreach (var discount in discounts)
+        {
+            string[] d = discount.ToArray();
+            int type = int.Parse(d[1]);
+            int value = int.Parse(d[2]);
+            if (!productDiscount.ContainsKey(d[0]))
+                productDiscount.Add(d[0], new Discount(d[0], type, value));
+        }
+
+        foreach (var product in products)
+        {
+            int price = int.Parse(product[0]);
+            int min = price;
+            for (int i = 0; i < product.Count; i++)
+            {
+                int currentPrice = price;
+                string index = product[i];
+                if (!string.Equals(index, "EMPTY", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (productDiscount.ContainsKey(index))
+                    {
+                        Discount discount = productDiscount[index];
+
+                        min = Math.Min(min, CalcDiscountAmount(currentPrice, discount));
+                    }
+                }
+
+            }
+            result += min;
+        }
+        return result;
+    }
+
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int productsRows = Convert.ToInt32(Console.ReadLine().Trim());
+        int productsColumns = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<string>> products = new List<List<string>>();
+
+        for (int i = 0; i < productsRows; i++)
+        {
+            products.Add(Console.ReadLine().TrimEnd().Split(' ').ToList());
+        }
+
+        int discountsRows = Convert.ToInt32(Console.ReadLine().Trim());
+        int discountsColumns = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<string>> discounts = new List<List<string>>();
+
+        for (int i = 0; i < discountsRows; i++)
+        {
+            discounts.Add(Console.ReadLine().TrimEnd().Split(' ').ToList());
+        }
+
+        int result = Result.findLowestPrice(products, discounts);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
---
+++ MeetingAssistant.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+public class MeetingAssistant
+{
+    public static T MaxValue<T>(params T[] values) where T : IComparable
+    {
+        return values.Max();
+    }
+
+    public static T MinValue<T>(params T[] values) where T : IComparable
+    {
+        return values.Min();
+    }
+
+    
+    public static string getEarliestMeetTime(List<string> events, int k)
+    {
+        // Create a dictionary to store the busy time intervals for each person
+        var busyTimes = new Dictionary<string, List<Tuple<DateTime, DateTime>>>();
+
+        // Parse the events and add the busy time intervals to the dictionary
+        foreach (var e in events)
+        {
+            var parts = e.Split();
+            var name = parts[0];
+            var startTime = DateTime.ParseExact(parts[2], "HH:mm", CultureInfo.InvariantCulture);
+            var endTime = DateTime.ParseExact(parts[3], "HH:mm", CultureInfo.InvariantCulture);
+            if (!busyTimes.ContainsKey(name))
+            {
+                busyTimes[name] = new List<Tuple<DateTime, DateTime>>();
+            }
+            busyTimes[name].Add(new Tuple<DateTime, DateTime>(startTime, endTime));
+        }
+
+        // Initialize the earliest start time and latest end time to the beginning and end of the day, respectively
+        var earliestStartTime = DateTime.ParseExact("00:00", "HH:mm", null);
+        var latestEndTime = DateTime.ParseExact("23:59", "HH:mm", null);
+        var firstIntervals = busyTimes.First().Value;
+        var secondIntervals = busyTimes.Last().Value;        
+        foreach (var firstInterval in firstIntervals)
+        {
+            bool isIntersecting = false;
+            foreach (var secondInterval in secondIntervals)
+            {
+                if (firstInterval.Item1 < secondInterval.Item2 && firstInterval.Item2 > secondInterval.Item1)
+                {
+                    // The two intervals intersect
+                    isIntersecting = true;
+                    break;
+                }
+
+                if (!isIntersecting)
+                {
+                    //nonIntersectionInterval = firstInterval;
+                    var gap = firstInterval.Item2 - firstInterval.Item1;
+                    if (gap >= TimeSpan.FromMinutes(k))
+                    {
+                        earliestStartTime = MaxValue<DateTime>(earliestStartTime, firstInterval.Item1);
+                        latestEndTime = MinValue<DateTime>(latestEndTime, firstInterval.Item2);
+                        /*break*/;
+                    }
+                    // Check if there is a time slot of k minutes available for all people
+                    if (earliestStartTime + TimeSpan.FromMinutes(k) <= latestEndTime)
+                    {
+                        return earliestStartTime.ToString("HH:mm");
+                    }
+                    
+                }
+            }
+        }
+        return "-1";
+    }
+
+    static void Main(string[] args)
+    {
+        string[] events = new string[] { "sam sleep 12:00 18:59", "alex gaming 00:00 11:00" };
+        List<string> list = events.ToList();
+        Console.WriteLine(getEarliestMeetTime(list, 60));
+    }
+}
