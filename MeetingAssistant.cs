f7c1805dc710f9aaee6fcb3bd5d7de8b5f7b3158
Mallikharjuna Rao <31948311+2mallikharjuna@users.noreply.github.com>
2024-10-23 17:09:06 +1100

    Update MeetingAssistant.cs

--- MeetingAssistant.cs
+++ MeetingAssistant.cs
@@ -1,27 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
 public class MeetingAssistant
 {
-    public static T MaxValue<T>(params T[] values) where T : IComparable
+    public static string GetEarliestMeetTime(List<string> events, int k)
     {
-        return values.Max();
+        // Parse events and build busy times
+        var busyTimes = ParseBusyTimes(events);
+
+        // Find earliest available meeting time
+        return FindEarliestMeetingTime(busyTimes, k);
     }
 
-    public static T MinValue<T>(params T[] values) where T : IComparable
+    private static Dictionary<string, List<Tuple<DateTime, DateTime>>> ParseBusyTimes(List<string> events)
     {
-        return values.Min();
-    }
-
-    
-    public static string getEarliestMeetTime(List<string> events, int k)
-    {
-        // Create a dictionary to store the busy time intervals for each person
         var busyTimes = new Dictionary<string, List<Tuple<DateTime, DateTime>>>();
 
-        // Parse the events and add the busy time intervals to the dictionary
         foreach (var e in events)
         {
             var parts = e.Split();
@@ -35,49 +31,39 @@ public class MeetingAssistant
             busyTimes[name].Add(new Tuple<DateTime, DateTime>(startTime, endTime));
         }
 
-        // Initialize the earliest start time and latest end time to the beginning and end of the day, respectively
-        var earliestStartTime = DateTime.ParseExact("00:00", "HH:mm", null);
-        var latestEndTime = DateTime.ParseExact("23:59", "HH:mm", null);
-        var firstIntervals = busyTimes.First().Value;
-        var secondIntervals = busyTimes.Last().Value;        
-        foreach (var firstInterval in firstIntervals)
-        {
-            bool isIntersecting = false;
-            foreach (var secondInterval in secondIntervals)
-            {
-                if (firstInterval.Item1 < secondInterval.Item2 && firstInterval.Item2 > secondInterval.Item1)
-                {
-                    // The two intervals intersect
-                    isIntersecting = true;
-                    break;
-                }
+        return busyTimes;
+    }
 
-                if (!isIntersecting)
-                {
-                    //nonIntersectionInterval = firstInterval;
-                    var gap = firstInterval.Item2 - firstInterval.Item1;
-                    if (gap >= TimeSpan.FromMinutes(k))
-                    {
-                        earliestStartTime = MaxValue<DateTime>(earliestStartTime, firstInterval.Item1);
-                        latestEndTime = MinValue<DateTime>(latestEndTime, firstInterval.Item2);
-                        /*break*/;
-                    }
-                    // Check if there is a time slot of k minutes available for all people
-                    if (earliestStartTime + TimeSpan.FromMinutes(k) <= latestEndTime)
-                    {
-                        return earliestStartTime.ToString("HH:mm");
-                    }
-                    
-                }
+    private static string FindEarliestMeetingTime(Dictionary<string, List<Tuple<DateTime, DateTime>>> busyTimes, int k)
+    {
+        var intervals = busyTimes.Values.SelectMany(i => i).OrderBy(interval => interval.Item1).ToList();
+        var dayStart = DateTime.ParseExact("00:00", "HH:mm", CultureInfo.InvariantCulture);
+        var dayEnd = DateTime.ParseExact("23:59", "HH:mm", CultureInfo.InvariantCulture);
+        
+        DateTime lastEndTime = dayStart;
+
+        foreach (var interval in intervals)
+        {
+            if (lastEndTime + TimeSpan.FromMinutes(k) <= interval.Item1)
+            {
+                return lastEndTime.ToString("HH:mm");
             }
+            lastEndTime = new[] { lastEndTime, interval.Item2 }.Max();
         }
-        return "-1";
+
+        // Check for time after the last meeting until the end of the day
+        if (lastEndTime + TimeSpan.FromMinutes(k) <= dayEnd)
+        {
+            return lastEndTime.ToString("HH:mm");
+        }
+
+        return "-1"; // No available time found
     }
 
     static void Main(string[] args)
     {
         string[] events = new string[] { "sam sleep 12:00 18:59", "alex gaming 00:00 11:00" };
         List<string> list = events.ToList();
-        Console.WriteLine(getEarliestMeetTime(list, 60));
+        Console.WriteLine(GetEarliestMeetTime(list, 60)); // Output: "11:00"
     }
 }
